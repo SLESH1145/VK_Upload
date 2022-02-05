91116407f8812761fd840e7a8568468eede2db9f
Tomek <MAGICSLESH@GMAIL.COM>
2022-02-05 17:32:33 +0300

    Update view

--- vk_downloader/main.py
+++ vk_downloader/main.py
@@ -2,26 +2,29 @@ import vk_api
 from config import token, group_id
 import os, time
 
-session = vk_api.VkApi(token= token)
+session = vk_api.VkApi(token=token)
 vk = session.get_api()
 
 
-def import_video():
-    directory = 'C:/Users/magic/Videos/Captures'
-    name_videos = os.listdir(directory)
+def import_video(file_folder):
+    print('Please wait...')
+    name_videos = os.listdir(file_folder)
     mp4 = filter(lambda x: x.endswith('.mp4'), name_videos)
     zip_paths = list(mp4)
     for path in zip_paths:
         upload = vk_api.VkUpload(session)
         name = path.removesuffix('.mp4')
         time.sleep(3)
-        download = upload.video(video_file=f'C:/Users/magic/Videos/Captures/{path}',
+        download = upload.video(video_file=f'{file_folder}/{path}',
                                 name=name,
                                 group_id=group_id)
-        print(path, download['video_id'])
+        print(f"[+] Video: {path}\n"
+              f"id: {download['video_id']} is loaded")
 
-import_video()
+def main():
+    path = "C:/Users/magic/Videos/Captures"
+    import_video(path)
+    print('Download finished')
 
-
-
-
+if __name__ == '__main__':
+    main()
